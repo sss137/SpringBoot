d40c3575b6133023793998d39299df5ee23dd48a
sss137 <tlstjdtn137@naver.com>
2025-09-22 17:46:10 +0900

    09.22

--- 07_Swagger/src/main/java/org/shark/boot07/user/controller/UserApiController.java
+++ 07_Swagger/src/main/java/org/shark/boot07/user/controller/UserApiController.java
@@ -7,13 +7,10 @@ import org.shark.boot07.user.dto.enums.SortType;
 import org.shark.boot07.user.dto.request.UserCreatedRequestDTO;
 import org.shark.boot07.user.dto.request.UserUpdateRequestDTO;
 import org.shark.boot07.user.dto.response.ApiUserResponseDTO;
-import org.shark.boot07.user.exception.ApiUserErrorResponseDTO;
-import org.shark.boot07.user.exception.UserNotFoundException;
 import org.shark.boot07.user.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,15 +35,14 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 @RestController
-
 /*
  * Swagger 설정 Annotation
- * @Tag
- * @Operation
- * @ApiResponse
- * @Parameter
+ *  @Tag
+ *  @Operation
+ *  @ApiResponse
+ *  @Parameter
  */
-@Tag(name = "User API", description = "사용자 관리 API")
+@Tag(name = "User API 목록", description = "사용자 관리 API")
 public class UserApiController {
 
   private final UserService userService;
@@ -54,113 +50,126 @@ public class UserApiController {
   /*
    * Postman 요청 시 주의사항
    * 1. x-www-form-urlencoded 형식으로 데이터를 보내는 경우 (폼을 사용하는 경우)
-   *    create(UserDTO user)
+   *   create(UserDTO user)
    * 2. raw 형식 중 JSON 데이터를 보내는 경우
-   *    create(@RequestBody UserDTO user)
+   *   create(@RequestBody UserDTO user)
    */
   @PostMapping
-  @Operation(summary = "신규 사용자 등록",
-             description = "아이디(이메일), 비밀번호, 닉네임을 이용하는 신규 사용자 등록 API")
+  @Operation(summary = "신규 사용자 등록"
+           , description = "아이디(이메일), 비밀번호, 닉네임을 이용하는 신규 사용자 등록 API")
   @ApiResponses(value = {
-      @ApiResponse(responseCode = "201",
-                   description = "사용자 등록 성공",
-                   content = @Content(schema = @Schema(implementation = ApiUserResponseDTO.class)))
+      @ApiResponse(responseCode = "201"
+                 , description = "사용자 등록 성공"
+                 , content = @Content(schema = @Schema(implementation = ApiUserResponseDTO.class)))
   })
-  public ResponseEntity<ApiUserResponseDTO> create(@Valid @RequestBody UserCreatedRequestDTO user) {
-    ApiUserResponseDTO createUser = ApiUserResponseDTO.builder()
-                                                .status(201)   //요청이 성공적으로 처리되었으며 새로운 자원이 생성되었습니다. //사람이 보는 용도
-                                                .message("사용자 등록이 성공했습니다.")
-                                                .results(Map.of("createdUser", userService.createUser(user.toUserDTO())))
-                                                .build();
-    return ResponseEntity.status(HttpStatus.CREATED).body(createUser);
+  public ResponseEntity<ApiUserResponseDTO> create(
+      @Valid @RequestBody UserCreatedRequestDTO user
+  ) {
+    ApiUserResponseDTO createdUser = ApiUserResponseDTO.builder()
+        .status(201)   //요청이 성공적으로 처리되었으며, 새로운 자원이 생성되었습니다.
+        .message("회원 등록이 성공했습니다.")
+        .results(Map.of("createdUser", userService.createUser(user.toUserDTO())))
+      .build();
+    return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
   }
   
   @PutMapping("/{uid}")
-  @Operation(summary = "사용자 정보 수정",
-             description = "비밀번호, 닉네임을 수정하는 신규 사용자 정보 등록 API")
+  @Operation(summary = "사용자 정보 수정"
+           , description = "비밀번호와 닉네임을 수정하는 사용자 정보 등록 API")
   @ApiResponses(value = {
-   @ApiResponse(responseCode = "200",
-                description = "사용자 정보 수정 성공",
-                content = @Content(schema = @Schema(implementation = ApiUserResponseDTO.class)))
+      @ApiResponse(responseCode = "200"
+                 , description = "사용자 정보 수정 성공"
+                 , content = @Content(schema = @Schema(implementation = ApiUserResponseDTO.class)))
   })
-  public ResponseEntity<ApiUserResponseDTO> update(@PathVariable(value = "uid") Long uid,
-                                                 @Valid @RequestBody UserUpdateRequestDTO user) {
+  public ResponseEntity<ApiUserResponseDTO> update(
+      @PathVariable(value = "uid") Long uid,
+      @Valid @RequestBody UserUpdateRequestDTO user
+  ) {
     ApiUserResponseDTO updatedUser = ApiUserResponseDTO.builder()
-                                                 .status(200)
-                                                 .message("사용자 정보가 수정되었습니다.")
-                                                 .results(Map.of("updatedUser", userService.updateUser(user.toUserDTO(), uid)))
-                                                 .build();
+                                                       .status(200)
+                                                       .message("회원 정보가 수정되었습니다.")
+                                                       .results(Map.of("updatedUser", userService.updateUser(user.toUserDTO(), uid)))
+                                                       .build();
     return ResponseEntity.status(HttpStatus.OK).body(updatedUser);
   }
   
   @DeleteMapping("/{uid}")
-  @Operation(summary = "사용자 정보 삭제",
-             description = "사용자 번호가 일치하는 사용자를 삭제하는 API")
+  @Operation(summary = "사용자 정보 삭제"
+  , description = "사용자번호가 일치하는 사용자를 삭제하는 API")
   @ApiResponses(value = {
-  @ApiResponse(responseCode = "200",
-               description = "사용자 정보 삭제 성공",
-               content = @Content(schema = @Schema(implementation = ApiUserResponseDTO.class)))
+      @ApiResponse(responseCode = "200"
+          , description = "사용자 정보 삭제 성공"
+          , content = @Content(schema = @Schema(implementation = ApiUserResponseDTO.class)))
   })
-  public ResponseEntity<ApiUserResponseDTO> delete(@PathVariable(value = "uid") Long uid) {
+  public ResponseEntity<ApiUserResponseDTO> delete(
+      @PathVariable(value = "uid") Long uid
+  ) {
     userService.deleteUser(uid);
     ApiUserResponseDTO dto = ApiUserResponseDTO.builder()
-                                         .status(200)
-                                         .message("사용자 정보가 삭제되었습니다.")
-                                         .build();
-    return ResponseEntity.ok(dto);   // = ResponseEntity.status(HttpStatus.OK).body(dto);  <<  //이게 더 좋은 코드
+                                               .status(200)
+                                               .message("회원 정보가 삭제되었습니다.")
+                                               .build();
+    return ResponseEntity.ok(dto);
   }
   
   @GetMapping("/{uid}")
-  @Operation(summary = "사용자 정보 조회",
-             description = "사용자 번호가 일치하는 사용자를 조회하는 API")
+  @Operation(summary = "사용자 정보 조회"
+           , description = "사용자번호가 일치하는 사용자를 조회하는 API")
   @ApiResponses(value = {
-  @ApiResponse(responseCode = "200",
-               description = "사용자 정보 조회 성공",
-               content = @Content(schema = @Schema(implementation = ApiUserResponseDTO.class)))
+      @ApiResponse(responseCode = "200"
+          , description = "사용자 정보 조회 성공"
+          , content = @Content(schema = @Schema(implementation = ApiUserResponseDTO.class)))
   })
-  public ResponseEntity<ApiUserResponseDTO> detail(@PathVariable(value = "uid") Long uid) {
+  public ResponseEntity<ApiUserResponseDTO> detail(
+      @PathVariable(value = "uid") Long uid
+  ) {
     ApiUserResponseDTO dto = ApiUserResponseDTO.builder()
-                                         .status(200)
-                                         .message("사용자 조회 성공")
-                                         .results(Map.of("foundUser", userService.getUserById(uid)))
-                                         .build();
+                                               .status(200)
+                                               .message("회원 조회 성공")
+                                               .results(Map.of("foundUser", userService.getUserById(uid)))
+                                               .build();
     return ResponseEntity.ok(dto);
   }
-    
+  
   @GetMapping
-  @Operation(summary = "사용자 목록 조회",
-             description = "요청 파라미터(page, size, sort)에 따른 목록을 조회하는 API")
+  @Operation(summary = "사용자 목록 조회"
+  , description = "요청 파라미터(page, size, sort)에 따른 사용자 목록을 조회하는 API")
   @ApiResponses(value = {
-  @ApiResponse(responseCode = "200",
-               description = "사용자 목록 조회 성공",
-               content = @Content(schema = @Schema(implementation = ApiUserResponseDTO.class)))
+      @ApiResponse(responseCode = "200"
+                 , description = "사용자 목록 조회 성공"
+                 , content = @Content(schema = @Schema(implementation = ApiUserResponseDTO.class)))
   })
   @Parameters(value = {
-      @Parameter(name = "page",
-                 required = false,
-                 description = "조회할 페이지의 번호",
-                 in = ParameterIn.QUERY),
-      @Parameter(name = "size",
-                 required = false,
-                 description = "한 페이지의 포함할 사용자 수",
-                 in = ParameterIn.QUERY),
-      @Parameter(name = "sort",
-                 required = false,
-                 description = "사용자 정령 방식",
-                 in = ParameterIn.QUERY,
-                 schema = @Schema(implementation = SortType.class))
-               
+      @Parameter(name = "page"
+               , required = true
+               , description = "조회할 페이지의 번호"
+               , in = ParameterIn.QUERY  // 쿼리스트링으로 전달되는 파라미터
+               , schema = @Schema(type = "integer", minimum = "1", example = "1"))
+    , @Parameter(name = "size"
+               , required = false
+               , description = "한 페이지에 포함할 사용자 수"
+               , in = ParameterIn.QUERY
+               , schema = @Schema(type = "integer", defaultValue = "20", example = "20"))
+    , @Parameter(name = "sort"
+               , required = false
+               , description = "사용자 정렬 방식"
+               , in = ParameterIn.QUERY
+               , schema = @Schema(implementation = SortType.class))
   })
-  public ResponseEntity<ApiUserResponseDTO> list(PageDTO pageDTO,
-                                                  @RequestParam(value = "sort", defaultValue = "DESC") String sort) {
+  public ResponseEntity<ApiUserResponseDTO> list(
+      @RequestParam(value = "page", required = true) int page
+    , @RequestParam(value = "size", defaultValue = "20") int size
+    , @RequestParam(value = "sort", defaultValue = "DESC") String sort
+  ) {
+    PageDTO pageDTO = new PageDTO();
+    pageDTO.setPage(page);
+    pageDTO.setSize(size);
     ApiUserResponseDTO dto = ApiUserResponseDTO.builder()
-                                            .status(200)
-                                            .message("사용자 목록 조회 성공")
-                                            .results(Map.of("userList", userService.getUserList(pageDTO, sort)))
-                                            .build();
-      return ResponseEntity.ok(dto);
+                                               .status(200)
+                                               .message("회원 목록 조회 성공")
+                                               .results(Map.of("userList", userService.getUserList(pageDTO, sort)))
+                                               .build();
+    return ResponseEntity.ok(dto);
   }
-
+  
 }
-
-
--- 07_Swagger/src/main/java/org/shark/boot07/user/exception/advice/UserApiControllerAdvice.java
+++ 07_Swagger/src/main/java/org/shark/boot07/user/exception/advice/UserApiControllerAdvice.java
@@ -8,12 +8,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-import org.springframework.web.servlet.resource.NoResourceFoundException;
-
-//import lombok.extern.slf4j.Slf4j;
 
 /**
  * ----------+--------------------------------------------
@@ -24,29 +22,17 @@ import org.springframework.web.servlet.resource.NoResourceFoundException;
  *           |   20 / 필수 값 누락
  *           |   21 / 입력 가능한 크기 벗어남
  *           |   22 / 잘못된 형식의 아이디(이메일) 입력
- *           |   30 / 경로 변수 누락
- *           |   31 / 경로 변수 타입 오류
- *           |   40 / 요청 파라미터 누락
- *           |   41 / 요청 파라미터 타입 오류
- *           |   42 / 요청 파라미터 값 오류
+ *           |   30 / 요청 파라미터 누락
+ *           |   40 / 경로 변수 및 요청 파라미터 타입 오류
+ *           |   41 / 요청 파라미터 값 오류
  * ----------+--------------------------------------------
  *  404      | Not Found : 해당 정보 없음
  * ----------+--------------------------------------------
  */
 
-//@Slf4j
-@RestControllerAdvice(assignableTypes = {UserApiController.class})   //UserApiController 컨트롤러만 적용
+@RestControllerAdvice(assignableTypes = {UserApiController.class})   //UserApiController 컨트롤러에만 적용
 public class UserApiControllerAdvice {
 
-  //@ExceptionHandler(UserNotFoundException.class)
-  //@ResponseStatus(HttpStatus.NOT_FOUND)   //404가 전달되는 부분
-  //public ErrorResponseDTO handleUserNotFoundException(UserNotFoundException e) {
-  //  return ErrorResponseDTO.builder()
-  //                          .status(404)   //사람이 보는 용도
-  //                          .errorMessage(e.getMessage())
-  //                          .build();
-  //}
-  
   //전달된 회원번호(uid)를 가진 회원이 존재하지 않는 경우
   @ExceptionHandler(UserNotFoundException.class)
   public ResponseEntity<ApiUserErrorResponseDTO> handleUserNotFoundException(UserNotFoundException e) {
@@ -56,38 +42,37 @@ public class UserApiControllerAdvice {
     case 2: errorDetailMessage = "회원 정보를 수정할 수 없습니다."; break;
     case 3: errorDetailMessage = "회원 정보를 삭제할 수 없습니다."; break;
     }
-    ApiUserErrorResponseDTO dto = ApiUserErrorResponseDTO.builder()
-                                                          .errorCode("404")   //사람이 보는 용도
-                                                          .errorMessage(e.getMessage())
-                                                          .errorDetailMessage(errorDetailMessage)
-                                                          .build();
-    return ResponseEntity.status(404).body(dto);   //404가 전달되는 부분
+    ApiUserErrorResponseDTO errorDTO = ApiUserErrorResponseDTO.builder()
+        .errorCode("404")
+        .errorMessage(e.getMessage())
+        .errorDetailMessage(errorDetailMessage)
+      .build();
+    return ResponseEntity.status(404).body(errorDTO);
   }
   
   //중복된 값 입력
   @ExceptionHandler(DuplicateKeyException.class)
-  public ResponseEntity<ApiUserErrorResponseDTO> handleDuplicateUserException(DuplicateKeyException e) {
+  public ResponseEntity<ApiUserErrorResponseDTO> handleDuplicateKeyException(DuplicateKeyException e) {
     ApiUserErrorResponseDTO errorDTO = ApiUserErrorResponseDTO.builder()
-                                       .errorCode("10")
-                                       .errorMessage("중복된 값 입력")
-                                       .errorDetailMessage("동일한 아이디가 이미 존재합니다.")
-                                       .build();
+        .errorCode("10")
+        .errorMessage("중복된 값 입력")
+        .errorDetailMessage("동일한 아이디가 이미 존재합니다.")
+      .build();
     return ResponseEntity.badRequest().body(errorDTO);
   }
   
-  //필수값 누락, 입력 가능한 크기 벗어남, 잘못된 형식의 아이디(이메일)가 입력된 경우
+  //필수 값 누락, 입력 가능한 크기 벗어남, 잘못된 형식의 아이디(이메일)가 입력된 경우
   @ExceptionHandler(MethodArgumentNotValidException.class)
   public ResponseEntity<ApiUserErrorResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
     String errorCode = null;
     String errorMessage = null;
     String errorDetailMessage = null;
     BindingResult bindingResult = e.getBindingResult();
-    
-    if (bindingResult.hasFieldErrors()) {
+    if (bindingResult.hasErrors()) {
       switch (bindingResult.getFieldError().getCode()) {
       case "NotBlank":
-        errorCode = "20"; errorMessage = "필수값 누락";
-        break;
+        errorCode = "20"; errorMessage = "필수 값 누락";
+        break;      
       case "Size":
         errorCode = "21"; errorMessage = "입력 가능한 크기 벗어남";
         break;
@@ -95,52 +80,53 @@ public class UserApiControllerAdvice {
         errorCode = "22"; errorMessage = "잘못된 형식의 아이디(이메일) 입력";
         break;
       }
-      
       errorDetailMessage = bindingResult.getFieldError().getDefaultMessage();
     }
-    
     ApiUserErrorResponseDTO errorDTO = ApiUserErrorResponseDTO.builder()
-                                                              .errorCode(errorCode)
-                                                              .errorMessage(errorMessage)
-                                                              .errorDetailMessage(errorDetailMessage)
-                                                              .build();
-    return ResponseEntity.badRequest().body(errorDTO);
-  }
- 
-  //경로 변수가 누락된 경우
-  @ExceptionHandler(NoResourceFoundException.class)
-  public ResponseEntity<ApiUserErrorResponseDTO> handleNoResourceFoundException(NoResourceFoundException e) {
-    ApiUserErrorResponseDTO errorDTO = ApiUserErrorResponseDTO.builder()
-                                                              .errorCode("30")
-                                                              .errorMessage("경로 변수 누락")
-                                                              .errorDetailMessage("사용자 정보를 식별하기 위한 경로 변수 값이 누락되었습니다.")
-                                                              .build();
+        .errorCode(errorCode)
+        .errorMessage(errorMessage)
+        .errorDetailMessage(errorDetailMessage)
+      .build();
     return ResponseEntity.badRequest().body(errorDTO);
   }
   
-  //경로 변수의 타입이 잘못된 경우
-  @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-  public ResponseEntity<ApiUserErrorResponseDTO> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+  //필수 요청 파라미터가 누락된 경우
+  @ExceptionHandler(MissingServletRequestParameterException.class)
+  public ResponseEntity<ApiUserErrorResponseDTO> handleNoResourceFoundException(MissingServletRequestParameterException e) {
     ApiUserErrorResponseDTO errorDTO = ApiUserErrorResponseDTO.builder()
-                                                              .errorCode("31")
-                                                              .errorMessage("경로 변수 타입 오류")
-                                                              .errorDetailMessage("사용자 정보를 식별값은 1 이상의 양의 정수입니다.")
-                                                              .build();
+        .errorCode("30")
+        .errorMessage("필수 요청 파라미터 누락")
+        .errorDetailMessage("목록 확인을 위한 page 정보는 필수입니다.")
+      .build();
     return ResponseEntity.badRequest().body(errorDTO);
   }
- 
+  
+  //경로 변수 및 요청 파라미터의 타입이 잘못된 경우
+  @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+  public ResponseEntity<ApiUserErrorResponseDTO> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+    String errorDetailMessage = null;
+    switch (e.getName()) {   //e.getName()은 타입 오류가 발생한 경로 변수나 요청 파라미터의 이름
+    case "uid": errorDetailMessage = "사용자 정보 식별값은 1 이상의 양의 정수입니다."; break;
+    case "page": errorDetailMessage = "page는 1 이상의 양의 정수입니다."; break;
+    case "size": errorDetailMessage = "size는 1 이상의 양의 정수입니다."; break;    
+    }
+    ApiUserErrorResponseDTO errorDTO = ApiUserErrorResponseDTO.builder()
+        .errorCode("40")
+        .errorMessage("경로 변수 및 요청 파라미터 타입 오류")
+        .errorDetailMessage(errorDetailMessage)
+        .build();
+    return ResponseEntity.badRequest().body(errorDTO);
+  }
+  
   //정렬 시 sort 파라미터에 ASC/DESC 이외의 값이 전달되는 경우
   @ExceptionHandler(BadSqlGrammarException.class)
-  public ResponseEntity<ApiUserErrorResponseDTO> handleBadSqlGrammarException(BadSqlGrammarException   e) {
+  public ResponseEntity<ApiUserErrorResponseDTO> handleBadSqlGrammarException(BadSqlGrammarException e) {
     ApiUserErrorResponseDTO errorDTO = ApiUserErrorResponseDTO.builder()
-                                                              .errorCode("42")
-                                                              .errorMessage("요청 파라미터 값 오류")
-                                                              .errorDetailMessage("사용자 정보 정렬 방식은 ASC 또는 DESC 중 하나입니다.")
-                                                              .build();
+        .errorCode("41")
+        .errorMessage("요청 파라미터 값 오류")
+        .errorDetailMessage("사용자 정렬 방식은 ASC 또는 DESC 중 하나입니다.")
+        .build();
     return ResponseEntity.badRequest().body(errorDTO);
   }
   
 }
-  
-
-
