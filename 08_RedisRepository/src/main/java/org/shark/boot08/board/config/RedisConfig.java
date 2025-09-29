b18cf18975dcd02bd6979192f4c750e45b0b9f34
sss137 <tlstjdtn137@naver.com>
2025-09-30 08:54:18 +0900

    09.30

---
+++ 08_RedisRepository/src/main/java/org/shark/boot08/board/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package org.shark.boot08.board.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisConfig {
+
+  @Value("${spring.data.redis.host}")  //----- spring.data.redis.host 프로퍼티 읽기
+  private String host;
+  
+  @Value("${spring.data.redis.port}")  //----- spring.data.redis.port 프로퍼티 읽기
+  private int port;
+  
+  @Bean
+  RedisConnectionFactory redisConnectionFactory() {
+    return new LettuceConnectionFactory(host, port);
+  }
+  
+}
